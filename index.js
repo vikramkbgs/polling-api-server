a879b33ebc0a6e697b298aa1fe91e62b99971f33
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2023-07-07 15:23:50 +0530

    feature: server is stepup

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+app.use("/", require("./routes"));
+
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+
+app.listen(port, (req, res) => {
+  console.log(`server running on port... ${port}`);
+});
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection establised"))
+  .catch((error) => console.log("MongoDB connection fail", error.message));
